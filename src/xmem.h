804a9e26ce7d0ee8141dd2e75338ea304880a026
winterrdog <winterrdog@protonmail.ch>
2024-04-26 19:45:50 +0300

    converted into single header library

--- src/xmem.h
+++ src/xmem.h
@@ -1,6 +1,10 @@
 /*
     MIT License
 
+    Originally developed by Robin Gareus <robin@gareus.org>
+
+    Copyright 2010 Robin Gareus <robin@gareus.org>
+    
     Copyright (c) 2024 winterrdog
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,6 +24,10 @@
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
+
+    - winterrdog: readapted xstrdup()
+    - winterrdog: added xfree()
+
 */
 
 #ifndef _OAUTH_XMALLOC_H
@@ -31,11 +39,60 @@
 #include <string.h>
 #include <sys/types.h>
 
-/* Prototypes for functions defined in xmalloc.c  */
+/* Prototypes for functions */
 void* xmalloc(size_t size);
 void* xcalloc(size_t nmemb, size_t size);
 void* xrealloc(void* ptr, size_t size);
 void xfree(void* p);
 const char* xstrdup(const char* s);
+
+static void* xmalloc_fatal(size_t size)
+{
+    if (size == 0)
+        return NULL;
+    fprintf(stderr, "Out of memory. exiting..!\n");
+    exit(1);
+}
+
+void* xmalloc(size_t size)
+{
+    void* ptr = malloc(size);
+    if (ptr)
+        return ptr;
+    return xmalloc_fatal(size);
+}
+
+void* xcalloc(size_t nmemb, size_t size)
+{
+    void* ptr = calloc(nmemb, size);
+    if (ptr)
+        return ptr;
+    return xmalloc_fatal(nmemb * size);
+}
+
+void* xrealloc(void* ptr, size_t size)
+{
+    void* p = realloc(ptr, size);
+    if (p)
+        return p;
+    return xmalloc_fatal(size);
+}
+
+const char* xstrdup(const char* src)
+{
+    size_t src_len = strlen(src) + 1;
+
+    char* dest = (char*)xmalloc(src_len);
+    memcpy(dest, src, src_len), dest[src_len - 1] = 0;
+
+    return (const char*)dest;
+}
+
+void xfree(void* p)
+{
+    if (p) {
+        free(p), p = NULL;
+    }
+}
 
 #endif
